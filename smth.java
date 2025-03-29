51f58ecbd2b9144322153ccc104d2c3f52aa887c
amine2089 <155729561+amine2089@users.noreply.github.com>
2025-03-29 21:27:32 +0100

    hihhihihihihihihihihi

--- smth.java
+++ smth.java
@@ -1,5 +1,6 @@
 public class smth {
     public static void main(String[] args) {
         System.out.println("blébléblé");
+        System.out.println("hahahahahhahahahahahahahahahahha");
     }
 }
